3997a51f205d4925d1752fdad68846e6a4b13e5f
AGN <itzme907@gmail.com>
2023-05-21 13:05:49 +0300

    Add function for returning operation result

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function divide(a, b) {
   if (a < 0 && b < 0) return 'Zero is not divisible!';
   return a / b;
 }
+
+const firstOperand = 0;
+const secondOperand = 0;
+const operator = '';
+
+const operate = (a, b, operation) => {
+  let result = 0;
+  switch (operation) {
+    case '+':
+      result = add(a, b);
+      break;
+
+    case '-':
+      result = subtract(a, b);
+      break;
+
+    case '*':
+      result = multiply(a, b);
+      break;
+
+    case '/':
+      result = divide(a, b);
+      break;
+
+    default:
+      return 'You did\'nt specify an operator';
+  }
+
+  return result;
+};
